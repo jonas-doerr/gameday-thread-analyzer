ca56eaea93e81207842003dd9e52df826d31e76b
jonas-doerr <jonasdoerr2@gmail.com>
2025-09-20 15:43:03 +0800

    keywords + player mentions

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ I'm building a tool to scrape the comments off a Reddit gameday thread, in which
 ## To-do list
 - build tool to scrape comments [DONE]
 - tokenize comments [DONE]
-    - remove some basic stopwords that spacy didn't catch
+    - remove some basic stopwords that spacy didn't catch [DONE]
 - group and analyze comments
 
 ## Concepts Learned
@@ -13,11 +13,15 @@ I'm building a tool to scrape the comments off a Reddit gameday thread, in which
 - learned some basics of the spacy library
 
 ## Analysis ideas
-- most mentioned names
-- positive or negative overall sentiment
-- most mentioned words
+- most mentioned names [DONE]
+- positive or negative overall sentiment 
+- most mentioned words [DONE]
 - common themes throughout the thread
+- guess whether or not the Packers won
 
 ## Journal
 ### 9.19
 I figured out that I can't just leak my Reddit app's secret, so it took my a long time to clear out my old commits and make a new environment that didn't reveal it to everybody. After that, I cleared out some bad words that came up and also some words that didn't get caught by my previous stopwords function inherent to spacy.
+
+### 9.20
+I ranked the keywords most commonly seen in the gameday thread. I kept adding new words to my stopwords list, because words like "look" or "good" are not as useful as words like "refs" or "Parsons" (a player name). I also added a list of the Packers roster and used it to look through the lemmatized words and see which players were mentioned the most often. There was some margin of error (for example, parson not counting for Micah Parsons), which I might fix in my next edit.
--- analyzer.py
+++ analyzer.py
@@ -4,6 +4,7 @@ import emoji
 import spacy
 from dotenv import load_dotenv
 import os
+from collections import Counter
 
 load_dotenv()
 
@@ -28,7 +29,7 @@ submission = reddit.submission(url=thread_link)
 print("fetching comments") #debug in case gets laggy
 submission.comments.replace_more(limit=10) #it takes a while if I take too many comments
 print("done fetching") #also for debugging purposes
-comments = [comment.body for comment in submission.comments.list()[:20]]
+comments = [comment.body for comment in submission.comments.list()]
 
 # preprocess text
 cleaned_comments = [preprocess_comment(c) for c in comments]
@@ -43,11 +44,41 @@ lemma_comments = [
     if not token.is_stop and token.is_alpha] # removes numbers and stopwords
 
 # stopwords that didn't get caught
-stopwords = ['m', 's', 'non', 've']
+stopwords = ['m', 's', 'non', 've', 'not', 'see', 'talk', 'game', 'play', 'like', 'get', 'good', 'bad']
 bad_words = ['fuck', 'shit', 'ass', 'bitch', 'damn']
 lemma_comments = [
     c[0] + '*' * (len(c) - 1) if c in bad_words else c
     for c in lemma_comments
     if c not in stopwords]
 
-print(lemma_comments)
+#find keywords
+ranked_keywords = Counter(lemma_comments).most_common(10)
+print(ranked_keywords)
+
+#check which players were mentioned the most
+packers_roster = [["Israel", "Abanikanda"], ["Deslin", "Alexandre"], ["Zayne", "Anderson"], ["Johnathan", "Baldwin"],
+    ["Aaron", "Banks"], ["Brant", "Banks"], ["Anthony", "Belton"], ["Warren", "Brinson"],
+    ["Chris", "Brooks"], ["Karl", "Brooks"], ["Javon", "Bullard"], ["Dalton", "Cooper"],
+    ["Edgerrin", "Cooper"], ["Brenton", "Cox"], ["Romeo", "Doubs"], ["Kingsley", "Enagbare"],
+    ["James", "Ester"], ["John", "FitzPatrick"], ["Rashan", "Gary"], ["Travis", "Glover"],
+    ["Matthew", "Golden"], ["Kamal", "Hadden"], ["Mecole", "Hardman"], ["Malik", "Heath"],
+    ["Nate", "Hobbs"], ["Ty'Ron", "Hopper"], ["Josh", "Jacobs"], ["Elgton", "Jenkins"],
+    ["Donovan", "Jennings"], ["Jamon", "Johnson"], ["Darian", "Kinnard"], ["Tucker", "Kraft"],
+    ["MarShawn", "Lloyd"], ["Jordan", "Love"], ["Isaiah", "McDuffie"], ["Xavier", "McKinney"],
+    ["Brandon", "McManus"], ["Mark", "McNamee"], ["Bo", "Melton"], ["Jacob", "Monk"],
+    ["Jordan", "Morgan"], ["Arron", "Mosby"], ["Luke", "Musgrave"], ["Isaiah", "Neyor"],
+    ["Nick", "Niemann"], ["Keisean", "Nixon"], ["Kitan", "Oladapo"], ["Collin", "Oliver"],
+    ["Matthew", "Orzech"], ["Micah", "Parsons"], ["Jayden", "Reed"], ["Sean", "Rhyan"],
+    ["Micah", "Robinson"], ["Will", "Sheppard"], ["Jaylin", "Simpson"], ["Ben", "Sims"],
+    ["Barryn", "Sorrell"], ["Nazir", "Stackhouse"], ["Pierre", "Strong"], ["Zach", "Tom"],
+    ["Clayton", "Tune"], ["Carrington", "Valentine"], ["Lukas", "Van Ness"], ["Eric", "Wilson"],
+    ["Christian", "Watson"], ["Tory", "Whittington"], ["Dontayvion", "Wicks"], ["Devonte", "Wyatt"]]
+packers_lowercase_roster = [[player.lower() for player in full_name] for full_name in packers_roster]
+mentioned_player = {}
+for c in lemma_comments:
+    for first, last in packers_lowercase_roster:
+        if c == first or c == last:
+            full_name = f"{first.title()} {last.title()}"
+            mentioned_player[full_name] = mentioned_player.get(full_name, 0) + 1
+
+print(mentioned_player)
